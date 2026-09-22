401b0c9249777bdee22e4e898421e59e7ab69346
Anna Platonova <33085889+AnaryAnn@users.noreply.github.com>
2026-09-22 09:36:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ def generate_digest_background(peer_id):
 
 @app.get("/")
 def health():
-    return {"ok":True, "service":"sychnaya-ohota-v7.6.3-lively-dots-temp-0.9-test-peer-2000000001"}
+    return {"ok":True, "service":"sychnaya-ohota-v7.6.4-lively-dots-temp-0.9-test-peer-2000000001"}
 
 @app.post("/sychevestnik")
 def sychevestnik_schedule():
